db182a51378e3c61e9193be8b74c2cdf1b0e9b51
Craig <indigomx9@gmail.com>
2022-04-12 20:21:31 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cart
+
+Initial setup of the project.  Created with React and Redux-Toolkit.
+
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus iusto quidem ad laudantium! Quia in et, assumenda vitae sed totam, rem amet ab ea iusto tempora obcaecati repellendus cum. Impedit.</p>
+        </React.Fragment>
+    );
+};
+
+
